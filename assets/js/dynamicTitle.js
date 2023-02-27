293a2d0855856272bb6cb5512bd91f1f007214b5
Afranio-Fonseca <afraniofonseca@painfulsmile.com.br>
2023-02-27 15:54:37 -0300

    Disabled dynamic title

--- assets/js/dynamicTitle.js
+++ assets/js/dynamicTitle.js
@@ -1,15 +1,9 @@
 window.onload = function () {
   const favicon = document.getElementById('favicon')
   const pageTitle = document.title
-  const attentionMessage = 'Come back'
 
   function toggle(isPageActive) {
-    if (isPageActive) {
       document.title = pageTitle
       favicon.href = './assets/images/dp_male.svg'
-    } else {
-      document.title = attentionMessage
-      favicon.href = './assets/images/folded.png'
-    }
   }
 }
